6605179c7ad80a7ff1e46322b4ad1e7d34914697
jinxin0731 <jin.xin4@northeastern.edu>
2023-04-07 22:07:43 -0700

    a9

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter')
 const app = express()
 app.use(cors())
 app.use(express.json());
