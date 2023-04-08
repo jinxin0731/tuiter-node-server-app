f486e60a598c3b4382e3165207d78dd65e03fe3d
jinxin0731 <jin.xin4@northeastern.edu>
2023-04-08 02:18:43 -0700

    a9

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-    tuit: String,
-    likes: Number,
-    liked: Boolean,
-}, {collection: 'tuits'});
 
+const schema = mongoose.Schema({
+        tuit: String,
+        likes: Number,
+        liked: Boolean,
+        dislikes: Number,
+        disliked: Boolean,
+        replies: Number,
+        retuits: Number,
+    },
+    {collection: 'tuits'});
 export default schema;
