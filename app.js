62318950182dcc9ac9745073eef5a7eefa5f409f
jinxin0731 <jin.xin4@northeastern.edu>
2023-04-09 02:03:40 -0700

    a9: git commit -m a9:

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ import TuitsController
 import cors from 'cors'
 
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/tuiter';
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 const app = express()
 app.use(cors())
